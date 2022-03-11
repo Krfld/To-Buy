7e90cccb155e9b3810df62da72a7e5fa52be63c4
Krfld <rodrigofldias@gmail.com>
2022-03-11 22:20:16 +0000

    .

--- to_buy/lib/views/products_view.dart
+++ to_buy/lib/views/products_view.dart
@@ -6,26 +6,35 @@ class ProductsView extends StatelessWidget {
 
   const ProductsView({Key? key}) : super(key: key);
 
+  final double elevation = 8;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(elevation: elevation, child: Icon(Icons.add), onPressed: () => null),
       appBar: AppBar(
+        elevation: elevation,
         title: Text('Products'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () => null,
-          ),
-        ],
+        actions: [IconButton(icon: Icon(Icons.settings), onPressed: () => null)],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text('data'),
-            Text('data'),
-          ],
-        ),
+      body: ListView.separated(
+        padding: EdgeInsets.all(16),
+        physics: BouncingScrollPhysics(),
+        separatorBuilder: (context, index) => Divider(),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: elevation,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            child: ListTile(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+              title: Text('Title $index'),
+              subtitle: Text('Subtitle'),
+              onTap: () => null,
+            ),
+          );
+        },
       ),
     );
   }
